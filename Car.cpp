91a2ed1d228ce66e96c6957ab15b1f568b1b0dc8
YYJOU <b3230606@ulive.pccu.edu.tw>
2025-06-10 22:40:40 +0800

    Delete Car.cpp

--- Car.cpp
+++
@@ -1,5 +0,0 @@
-#include "Car.h"
-
-Car::Car(const std::string& p, bool m) : plate(p), monthly(m) {}
-std::string Car::getPlate() const { return plate; }
-bool Car::isMonthly() const { return monthly; }
